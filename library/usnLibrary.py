4be04da2cb6b0467381d969229dad739183cb65e
przemys1aw <pbojczuk@zoho.com>
2020-06-21 19:08:52 +0200

    def update

--- library/usnLibrary.py
+++ library/usnLibrary.py
@@ -1,10 +1,21 @@
 import apt
 
-def checkPackage(package):
+def checkPackageUpgrade(package):
   aptCache = apt.cache.Cache()
   for p in aptCache:
     if p.is_installed:
       if p.name == package:
         if p.versions[0].version > p.installed.version:
+          return package
+          break
+
+def checkPackageVersion(package, version):
+  aptCache = apt.cache.Cache()
+  for p in aptCache:
+    if p.is_installed:
+      if p.name == package:
+        print(p.installed.version)
+        print(version)
+        if p.installed.version < version:
           return package
           break
