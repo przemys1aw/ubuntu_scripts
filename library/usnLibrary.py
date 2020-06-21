b33d8b8abced8ecace7061235c26d345b31c4f4b
przemys1aw <pbojczuk@zoho.com>
2020-06-21 18:46:44 +0200

    library dir created

---
+++ library/usnLibrary.py
@@ -0,0 +1,10 @@
+import apt
+
+def checkPackage(package):
+  aptCache = apt.cache.Cache()
+  for p in aptCache:
+    if p.is_installed:
+      if p.name == package:
+        if p.installed.version < package:
+          if p.versions[0].version > p.installed.version:
+            return package
